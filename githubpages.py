c961b7717e92a03f4473231ca99e0724795cdd7a
Mynasru <30989850+Mynasru@users.noreply.github.com>
2019-01-13 14:47:10 +0100

    update fork from base (#1)
    * Implement custom sensor platform
    
    * Update
    
    * HassIO -> Hass.io
    
    * More custom component guides
    
    * Generic custom component
    
    * Updates
    
    * CSE7766 Update Interval (#91)
    
    * Update docs
    
    * Fixes
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Documentation for MAX31855 sensor (#97)
    
    * MAX 31855 documentation
    
    * Fix link
    
    * Update image
    
    * Fixed typo in Sonoff R2 Cover example (#112)
    
    * Privacy Policy
    
    * Upgrade footer
    
    * Remove line
    
    * Fix copy/paste error in turn_off_action (#113)
    
    * Redirects
    
    * Update docs
    
    * total_daily_energy doesn't have pin option (#114)
    
    * Getting started HassIO - USD device discovery (#110)
    
    I had a bad cable, although it works for charging devices, it didn't work for discovering a wemos D1 on HassIO with RPI3
    
    * Mismatch in example and text (#109)
    
    The example uses Dehumidifier the text said relay
    
    * BME280 environment cookbook entry (#107)
    
    * bme280 environment cookbook entery
    
    new cookbook entery fixing this issue:  https://github.com/OttoWinter/esphomedocs/issues/89
    
    * Formatting
    
    * Fixed formatting, removed global variables
    
    Fixed formatting, removed global variables as requested by OttoWinter
    
    * Comply with google C++ style guide
    
    * Fixed brackets
    
    * Tested and fixed typos/formatting
    
    Yalm expamples were tested by Mihalski
    
    * Update getting_started_command_line.rst (#102)
    
    Use ${PWD} to set current directory are works on both Powershell and Linux
    
    * Updated the multi click example code block (#92)
    
    I had no luck triggering the "Single Long Clicked" with the timing of
    - OFF for 1s to 2s
    - ON for 1s to 2s
    - OFF for at least 0.5s
    I removed the waiting first time of OFF for, and now i can trigger it as expected.
    
    * Add step to setting up devices (#101)
    
    * Add step to setting up devices
    
    Added activation of the MiFlora sensor with the native app.
    
    * Formatting
    
    * Fix
    
    * Update bme280_environment.rst
    
    * Bump version to v1.10.0b1
    
    * Fix CNAME
    
    * Update changelog
    
    * Fix CNAME
    
    * Add interval docs
    
    * Add changelog breaking/notable changes+imgtable
    
    * Bump version to v1.10.0b2
    
    * Update
    
    * Update v1.10.0.rst
    
    * Update v1.10.0.rst
    
    * Bump version to v1.10.0
    
    * Fix substitutions example & api intro text (#117)
    
    * Fix substitutions example
    
    * Remove wrong intro text
    
    * Fix spelling
    
    * Fixed missing link to BME cookbook (#120)
    
    Fixed missing link to BME cookbook
    
    * Update index.rst

--- githubpages.py
+++ githubpages.py
@@ -7,8 +7,12 @@ def create_nojekyll(app, env):
 
         path = os.path.join(app.builder.outdir, 'CNAME')
         with open(path, 'wt') as f:
-            f.write(os.getenv('CNAME', 'esphomelib.com'))
+            f.write(app.env.config.cname)
 
+        if 'beta' in app.env.config.cname:
+            with open(os.path.join(app.builder.outdir, 'robots.txt'), 'wt') as f:
+                f.write('User-agent: *\nDisallow: /\n')
 
 def setup(app):
+    app.add_config_value('cname', 'esphomelib.com', 'html')
     app.connect('env-updated', create_nojekyll)
